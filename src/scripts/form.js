7b1c4ac7caea0e4e10d78acf5fb6740a2f9300c2
Sergio Espitia <sdespitia@cvu.com.co>
2025-07-01 17:56:41 -0500

    completado version desk

---
+++ src/scripts/form.js
@@ -0,0 +1,34 @@
+const btnCol = document.getElementById('col');
+const btnRef = document.getElementById('ref');
+
+const imgCol = document.getElementById('img-2');
+const imgRef = document.getElementById('img-1');
+
+const contCol = document.getElementById('cont-col');
+const contRef = document.getElementById('cont-ref')
+
+btnCol.addEventListener('click', ()=>{
+
+    btnCol.classList.add('active-form');
+    btnRef.classList.remove('active-form');
+
+    imgCol.classList.remove('hidden');
+    imgRef.classList.add('hidden');
+
+    contCol.classList.remove('hidden');
+    contRef.classList.add('hidden')
+
+})
+
+btnRef.addEventListener('click', ()=>{
+
+    btnCol.classList.remove('active-form');
+    btnRef.classList.add('active-form');
+
+    imgCol.classList.add('hidden');
+    imgRef.classList.remove('hidden');
+
+    contCol.classList.add('hidden');
+    contRef.classList.remove('hidden')
+
+})
